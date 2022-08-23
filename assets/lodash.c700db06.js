c091d89005eb60fe66f0164dde0b7000599fbe0e
aparstechteam <79406959+aparstechteam@users.noreply.github.com>
2022-08-23 10:45:02 +0600

    test yourself stop

--- assets/lodash.c700db06.js
+++ assets/lodash.c700db06.js
@@ -1,4 +1,4 @@
-import{k as jt}from"./index.1f6764a4.js";var Ri={exports:{}};/**
+import{k as jt}from"./index.f6f6555a.js";var Ri={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
