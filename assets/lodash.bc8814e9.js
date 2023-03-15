cc9be2a62141232af7a6f12371ce1a04e93b3259
aparstechteam <79406959+aparstechteam@users.noreply.github.com>
2023-03-15 22:00:03 +0600

    dynamic score

--- assets/lodash.bc8814e9.js
+++ assets/lodash.bc8814e9.js
@@ -1,4 +1,4 @@
-import{N as jt}from"./index.9f3aa31d.js";var Ri={exports:{}};/**
+import{N as jt}from"./index.58fd59ec.js";var Ri={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
